b4581171ca27ab4ec1bc57ca1d25b1035c158477
coolsharp <coolsharp@gmail.com>
2016-11-11 01:12:40 +0900

    파일 정리

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'startbootstrap-sb-admin-master/api.html', {'posts': posts})
 
 
 def post_detail(request, pk):
